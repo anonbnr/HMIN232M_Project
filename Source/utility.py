512506ca7a3c920e5299ab8a212b8ada4e6bc743
shaqura <43540139+shaqura@users.noreply.github.com>
2019-03-28 11:54:03 +0100

    Update utility.py

--- Source/utility.py
+++ Source/utility.py
@@ -13,6 +13,15 @@ from sklearn.metrics import accuracy_score
 
 np.random.seed(500)
 
+from nltk.stem import WordNetLemmatizer
+tokens = word_tokenize(phrases[0])
+tokens = [w.lower() for w in tokens]
+print ("Lemmatisation \n")
+wordnet_lemmatizer = WordNetLemmatizer()
+lstemmed = [wordnet_lemmatizer.lemmatize(word,pos='v') for word in tokens]
+print("Lemmatisation : \n",lstemmed)
+
+
 #Prepare Train and Test Data sets
 
 Train_X, Test_X, Train_Y, Test_Y = model_selection.train_test_split(df['text_final'],df['label'],test_size=0.3)
@@ -23,14 +32,17 @@ Train_Y = Encoder.fit_transform(Train_Y)
 Test_Y = Encoder.fit_transform(Test_Y)
 
 #Word Vectorization
-Tfidf_vect = TfidfVectorizer(max_features=5000)
-Tfidf_vect.fit(df['text_final'])
-Train_X_Tfidf = Tfidf_vect.transform(Train_X)
-Test_X_Tfidf = Tfidf_vect.transform(Test_X)
+def Word_Vector(Train_X,Test_X)
+    Tfidf_vect = TfidfVectorizer(max_features=5000)
+    Tfidf_vect.fit(df['text_final'])
+    Train_X_Tfidf = Tfidf_vect.transform(Train_X)
+    Test_X_Tfidf = Tfidf_vect.transform(Test_X)
+    
+    return (Tfidf_vect, Train_X_Tfidf,Test_X_Tfidf)
 
-print(Tfidf_vect.vocabulary_)
-
-print(Train_X_Tfidf)
+word_vector= Word_Vector(Train_X,Test_X)
+print(word_vector[0].vocabulary_)
+print(word_vector[2])
 
 
 #The SVM — Support Vector Machine
